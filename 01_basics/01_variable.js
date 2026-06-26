b3ac0ba1f9c30adbe05eca3f9217ca5ad5d1844e
abubakar2119 <abubakar.dev2119@gmail.com>
2026-06-26 11:25:48 +0000

    datatype commit done

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,14 @@
+const accountId=12123
+let accountEmail="a1212@gmail,com"
+var accountpassword="11222"
+accountcity="kasur"
+let accountstate;
+
+
+// const accountId=2
+
+accountEmail="abubakar@gmail,com"
+accountpassword="1125"
+accountcity="lahore"
+console.log(accountId);
+console .table([accountEmail,accountId,accountpassword,accountcity,accountstate ])
---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,27 @@
+"use strict"//traat all js code as a newer version
+
+// alert ("hello")// we are using node js not browser
+
+console.log(3+3)
+
+//code read ability should be high 
+
+console.log("abubakar")
+
+let name="abubkar"
+let age=19
+let insignned="false"
+
+// datatypes
+//number  2 to the power 52
+//bigint
+//boolean  =>true/false
+//string  ""
+//null standa lone value
+//undefined   no define
+//symbol=>unique
+
+//object
+
+
+console.log(typeof undefined);
