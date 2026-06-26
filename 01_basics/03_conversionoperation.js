a0823e561fb929af3f454912a5c91615e0b57deb
abubakar2119 <abubakar.dev2119@gmail.com>
2026-06-26 16:08:59 +0000

    numberconversion commit

--- 01_basics/03_conversionoperation.js
+++ 01_basics/03_conversionoperation.js
@@ -14,16 +14,44 @@ let score=false
 //"23anaj"=>NaN(not a number)
 //true=1,false=0
 
-let isLoggedIn=1
-let booleanIsLoggedIn=Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
+// let isLoggedIn=1
+// let booleanIsLoggedIn=Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
 
 //1=>true;0=>false
 //""=>false
 //"abubakar"=>true
 
-let someNumber=55
+// let someNumber=55
 
-let stringNumber=String(someNumber)
-console.log( stringNumber)
-console.log(typeof stringNumber)
+// let stringNumber=String(someNumber)
+// console.log( stringNumber)
+// console.log(typeof stringNumber)
+
+
+//*********************operation***************** */
+
+// let value=3
+// let negvalue=-value
+// console.log(negvalue);
+
+// let str1 = "abubakar mustafa"
+// let str2 = "   hello m"
+
+// let str3 = str1+str2
+// console.log(str3);
+
+// console.log(1+2-"7");
+// console.log("1"+9);
+// console.log(3*"4");
+// console.log(3+2%4-6);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1,num2,num3;
+num1=num2=num3=2+2;
+
+let gamecounter=100;
+gamecounter++;
+console.log(gamecounter);
