e15b52ca759d421c38282c406b7cd3a16bd2aafb
abubakar2119 <abubakar.dev2119@gmail.com>
2026-06-26 15:16:54 +0000

    coversion

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,29 @@
+let score=false
+
+// console.log(typeof score)
+
+// console.log(typeof (score))
+
+// let valueInNumber=Number(score)
+
+// console.log(typeof valueInNumber)
+
+
+// console.log(valueInNumber )
+//"33"=>33
+//"23anaj"=>NaN(not a number)
+//true=1,false=0
+
+let isLoggedIn=1
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+//1=>true;0=>false
+//""=>false
+//"abubakar"=>true
+
+let someNumber=55
+
+let stringNumber=String(someNumber)
+console.log( stringNumber)
+console.log(typeof stringNumber)
