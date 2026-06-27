bfefc1c1e7cbf64360ff1c442d9e1b42350445a7
abubakar2119 <abubakar.dev2119@gmail.com>
2026-06-27 06:07:52 +0000

    comparison commit done

---
+++ 01_basics/04_comparsion.js
@@ -0,0 +1,24 @@
+// console.log(2<4);
+// console.log(2<=4);
+// console.log(2>4);
+// console.log(2>=4);
+// console.log(2==4);
+// console.log(2!=4);
+
+// console.log("2">=1);
+// console.log(0>="1");
+
+// console.log(null<=0);
+// console.log(null==0);
+// console.log(null>=0);
+
+
+//===  =>  check data type 
+
+console.log("2"===2);
+
+
+
+
+
+
---
+++ 01_basics/datatype_summary.js
@@ -0,0 +1,37 @@
+//primittive
+
+
+//7types: string,boolean,null,undefined,symbol,object,bigint
+// const score=100
+// const anotherscore=100.4
+
+// const IsLoggedIn=false
+// const outsidetemp=null
+// let userId;
+
+const  Id=Symbol("123")
+const anotherId=Symbol("123")
+console.log(Id===anotherId);
+// const BigNumber=2234567890n
+
+
+//(referance) non primittive
+
+//Array, object,function
+
+const heros=["ali" ,"talha"]
+{
+
+    const name="abubakar";
+    const age=18
+
+
+}
+
+const myfunction=function(){
+    console.log("hello world");
+    
+}
+console.log( typeof heros);
+
+
