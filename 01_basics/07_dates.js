71f10e46ea34436f151a1ce9cab37a2729381f54
abubakar2119 <abubakar.dev2119@gmail.com>
2026-06-28 12:13:14 +0000

    dates commit

---
+++ 01_basics/07_dates.js
@@ -0,0 +1,51 @@
+//dates
+
+let myDate= new Date();
+
+// console.log(myDate.toString());
+
+// console.log(myDate.toJSON());
+
+// console.log(myDate.toDateString());
+
+// console.log(myDate.toLocaleDateString());
+
+// console.log(myDate.toLocaleString());
+
+// console.log(myDate.toTimeString());
+
+// console.log(typeof myDate);
+
+// const anotherDate = new Date(2026,3,23,3,54);
+// const anotherDate = new Date("2026-02-14");
+// const anotherDate = new Date("03-12-2025");
+// // console.log(anotherDate.toString());
+// console.log(anotherDate.toLocaleString());
+
+// let myDateStamp=Date.now()
+
+// console.log(myDateStamp)
+
+// console.log(anotherDate.getTime())
+
+// console.log(Date.now());
+
+
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate=new Date()
+// console.log(newDate);
+
+// console.log(newDate.getDate());
+// console.log(newDate.getHours());
+
+// `${newDate.getHours()}and time is`
+
+
+
+console.log(newDate.toLocaleString('default', {
+
+    weekday:"narrow",
+}));
+
+
