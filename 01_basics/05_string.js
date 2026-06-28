80174ad2944f036219f3da5dcd476e5861276dbf
abubakar2119 <abubakar.dev2119@gmail.com>
2026-06-28 07:13:20 +0000

    string commit

---
+++ 01_basics/05_string.js
@@ -0,0 +1,35 @@
+// const name="abubakar"
+// const repo=30;
+// console.log(name+repo+"value");
+
+// console.log(`hello my name is ${name} and my repo is ${repo}`);
+
+ const gameName=new String("abubakar-musta-fa")
+// console.log(gameName[0]);
+
+// console.log(gameName.charAt(2));
+
+// console.log(gameName.indexOf("k"));
+
+
+// const newstring=gameName.substring(0,4)
+
+
+// console.log(newstring);
+
+const anotherString = gameName.slice(-7, 3);
+console.log(anotherString);
+
+const newName="      abubakar   "
+console.log(newName);
+console.log(newName.trim());
+
+
+const url="https://www.google.com/abubakar@20%abubakar.com"
+console.log(url.replace('20%','-'));
+
+console.log(url.includes('sundar'));
+
+console.log(gameName.split('-'));
+
+
