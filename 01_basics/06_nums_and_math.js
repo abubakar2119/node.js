b3400577d32b83175135dfb83e23982f037b13e7
abubakar2119 <abubakar.dev2119@gmail.com>
2026-06-28 08:36:49 +0000

    mums and math commit

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,38 @@
+const score=400
+// console.log(score);
+
+const name = new Number(400);
+
+// console.log(name);
+
+// console.log(name.toString().length);
+// console.log(name.toFixed(2));
+
+// console.log(typeof "name");
+
+const anothernamre = 239.7678;
+
+// console.log(anothernamre.toPrecision(3));
+
+// const hundreds=1000000;
+// console.log(hundreds .toLocaleString("en-IN"));
+
+//=================Maths====================
+
+// console.log(Math)
+// console.log(Math.abs(-6));
+
+// console.log(Math.round(4.7));
+// console.log(Math.ceil(4.1));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(4,2,25,1));
+// console.log(Math.max(4,2,25,1));
+
+
+console.log(Math.random());
+console.log((Math.random())*10 + 1);
+console.log(Math.floor(Math.random())*10 + 1);
+
+const max=20;
+const min=10;
+console.log(Math.floor(Math.random() * (max-min+1))+min);
