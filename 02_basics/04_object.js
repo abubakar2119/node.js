83e09466614d6d63617adde3de5feb93757b03a4
abubakar2119 <abubakar.dev2119@gmail.com>
2026-07-02 08:31:10 +0000

    object commit

---
+++ 02_basics/04_object.js
@@ -0,0 +1,54 @@
+const tinderuser = new Object();
+// const tinderuser={}
+
+tinderuser.id="123abc"
+tinderuser.name="abuabakar"
+tinderuser.isLoggedIn=false
+
+
+// console.log(tinderuser);
+
+const firstUser={
+    userEmail:"LSJKA@gmail.com",
+    userfullname:{
+        userfirstname:"abubakar",
+        userlastname:"mustafa"
+
+    }
+}
+// console.log(firstUser.userfullname.userlastname);
+
+const obj1={1:"a",4:"s"}
+const obj2={2:"a",5:"s"}
+const obj3={3:"a",6:"s"}
+
+// const obj4=Object.assign({},obj1,obj2,obj3)
+const obj4={...obj1,...obj2,...obj3}
+// console.log(obj4);
+
+const user=[
+    
+{
+    id:"1222",
+    email:"anaj@gmail.com"
+},
+{
+    id:"1222",
+    email:"anaj@gmail.com"
+},
+{
+    id:"1222",
+    email:"anaj@gmail.com"
+},
+
+]
+user[1].email
+// console.log(tinderuser);
+
+// console.log(Object.keys(tinderuser));
+// console.log(Object.entries(tinderuser));
+// console.log(Object.values(tinderuser));
+
+console.log(tinderuser.hasOwnProperty('name'));
+
+
