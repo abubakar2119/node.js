3500a2cb3621f2f25e04b62a40c3d83e9f0e146e
abubakar2119 <abubakar.dev2119@gmail.com>
2026-07-11 10:44:53 +0000

    function 2

--- 03_basics/01_function.js
+++ 03_basics/01_function.js
@@ -34,12 +34,44 @@ const result =addnum(3,6)
 
 function loginusermessage(username="saim") {
     if(username=== undefined){
-        console.log("please enter a user name");
+        // console.log("please enter a user name");
         return  
     }
     return `${username} just logged in`
     
 }
 //  console.log (loginusermessage("abubakar"))
- console.log (loginusermessage("abubakar"))
+//  console.log (loginusermessage("abubakar"))
 
+//++++++++++++++++++++//
+
+function calculatecartprice(val1,val2,...number1){
+    return number1
+
+}
+// console.log(calculatecartprice(200,304,400,500));
+const user={
+    username:"abubakar",
+    prices:459,
+
+}
+function handleobject(anyobject) {
+    // console.log(`username is ${anyobject.username} and price is ${anyobject.price} `)
+}
+
+// handleobject(user)
+
+handleobject({
+    name:"abubakar",
+    price:323
+
+})
+
+function returnSecondValue(getArray) {
+    return getArray[1];
+}
+
+const mynewArray = [200, 400, 100, 600];
+
+// console.log(returnSecondValue(mynewArray));
+console.log(returnSecondValue([200,400,100,600]));
