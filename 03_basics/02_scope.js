cc33fa3a908bd424c5af84e7555f2b0ea359a5de
abubakar2119 <your-github-email@example.com>
2026-07-11 19:12:15 +0500

    scope level

--- 03_basics/02_scope.js
+++ 03_basics/02_scope.js
@@ -5,9 +5,52 @@ if (true) {
     const a = 10
     let b = 20
     // var c = 30
-console.log("inner block", a, b, c)
+// console.log("inner block", a, b, c)
 }
 
 // console.log(a)
-console.log(b)  
+// console.log(b)  
 // console.log(c)
+
+
+function myfunction() {
+  const name = "abubakar"
+  
+
+  function myinnerfunction() {
+    const website = "abubakar.com"
+    // console.log(name)
+  }
+//   console.log(website)  
+
+myinnerfunction()
+
+}
+myfunction()
+
+if (true) {
+    const name = "abubakar"
+  if (name === "abubakar") {
+    const website = "     google.com"
+    //console.log(name + website)
+  }
+//   console.log(website)
+}   
+
+
+// console.log(name)  
+
+
+//+++++++++++interseting example++++++++++++++//
+// console.log(addone(5))
+
+// function addone(number) {
+// return number + 1
+
+// }
+
+addtwo(5)
+
+const addtwo = function (num) {
+ return num + 2   
+}
