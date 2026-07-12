bbe5c370871caa4f6bdff34605771b000fc6b5c2
abubakar2119 <your-github-email@example.com>
2026-07-12 20:09:11 +0500

    two

---
+++ 05_iteration/two.js
@@ -0,0 +1,25 @@
+// //while 
+
+// let i = 2;
+// while (i <= 10){
+//     console.log(`value of i is ${i}`);
+//     i=i+2;
+// }
+
+// let myArray = ["spider", "batman", "superman", "ironman", "hulk"];
+// let index = 0;
+// while (index < myArray.length){
+//     console.log(`value of index is ${myArray[index]}`);
+//     index++;
+// }   
+
+//do while loop
+
+do{
+    
+
+}while (i <= 10)
+
+
+
+
